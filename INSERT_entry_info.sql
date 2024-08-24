8bdb6d52624d238fb9f4155c53b7ac743c680123
t.masumoto <yef9b3ea72kujqun@gmail.com>
2024-08-25 01:55:25 +0900

    create table追加

--- INSERT_entry_info.sql
+++ INSERT_entry_info.sql
@@ -1,3 +1,25 @@
+create table entry_info (
+    entry_id varchar(10),
+    req_date varchar(8),
+    entry_date varchar(8),
+    entry_time_s integer(2),
+    entry_time_e integer(2),
+    req_user varchar(5),
+    req_group varchar(2),
+    entry_user1 varchar(5),
+    entry_user2 varchar(5),
+    entry_user3 varchar(5),
+    entry_user4 varchar(5),
+    entry_user5 varchar(5),
+    req_reason varchar(1000),
+    remark varchar(1000),
+    status varchar(1),
+    conform_user1 varchar(5),
+    conform_user2 varchar(5),
+    conform_user3 varchar(5)
+    primary key (entry_id,req_date)
+);
+
 INSERT INTO entry_info (
     entry_id, req_date, entry_date, entry_time_s, entry_time_e, req_user, req_group, 
     entry_user1, entry_user2, entry_user3, entry_user4, entry_user5, 
